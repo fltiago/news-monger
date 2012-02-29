3bd1acc5bcab75d6ae71067c6776c3496b68de4f
fltiago <fltiago@gmail.com>
2012-02-29 09:54:27 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewsMonger::Application.config.secret_token = '3251fe26721500f6861fb39d1dba91535338d488a04a717b99a8ec879a89891eb7592808c1495a09affbb1eac3735206f1ac449839751c47961f2a118e26d823'
